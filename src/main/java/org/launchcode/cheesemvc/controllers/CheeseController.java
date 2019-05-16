11ce1d06ffe1bdaacfd72173334267424c02c99a
Amanda Clark <aaclark15@yahoo.com>
2019-05-16 12:49:27 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -0,0 +1,45 @@
+package org.launchcode.cheesemvc.controllers;
+
+
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+
+
+@Controller
+@RequestMapping("cheese") //makes base path /cheese
+public class CheeseController {
+
+    //putting list outside of method makes it global
+    //using arraylist for data is temp - goes away when server stops
+    static ArrayList<String> cheeses = new ArrayList<>(); //creates a new list
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("cheeses", cheeses); //passes our list to our template
+        model.addAttribute("title", "My Cheeses");
+        return "cheese/index";
+    }
+
+    //new method to make Add form - the display
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    //create handler to process the form
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    //use @RequestParam as parameter to pull data from form - look for variable (needs to match form)
+    public String processAddCheeseForm(@RequestParam String cheeseName) {
+        cheeses.add(cheeseName);
+        return "redirect:"; //redirect to /cheese (base path)
+    }
+}
